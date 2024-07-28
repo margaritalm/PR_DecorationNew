5bfc4588d0f67b047efec40e0eb16f859471ce47
Margarita <margarita.levitm@checkmarx.com>
2024-07-28 17:48:56 +0300

    Update BookDetail_jsp.java

--- BookDetail_jsp.java
+++ BookDetail_jsp.java
@@ -163,11 +163,11 @@ public class BookDetail_jsp extends HttpJspBase {
     }
   }
   //werkjwoeriwoeriw
-  String getParam(javax.servlet.http.HttpServletRequest req, String paramName) {
-    String param = req.getParameter(paramName);
-    if ( param == null || param.equals("") ) return "";
-    return param;
-  }
+ 
+ 
+    
+  
+
 
   boolean isNumber (String param) {
     boolean result;
